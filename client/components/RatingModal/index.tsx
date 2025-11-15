5e3a516b592a9e722b7cdf3c4b5590a8d79534b2
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-11-16 01:48:36 +0700

    feat(component): add rating modal

--- client/components/RatingModal/index.tsx
+++ client/components/RatingModal/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { UserRating } from "../../../../shared/src/types";
+import { UserRating } from "../../../shared/src/types";
 
 interface RatingModalProps {
   rating: UserRating | null;
