3dd011695d70a48b1d1dbb36e9a2f35cc5d87c3b
huynhgiaau27112005 <huynhgiaau27112005@gmail.com>
2025-11-16 09:33:10 +0700

    temp

--- client/components/ProductCard/index.tsx
+++ client/components/ProductCard/index.tsx
@@ -7,10 +7,10 @@ import Image from "next/image"
 
 export default function ProductCard({ product } : { product: Product }) {
   return <div className="group relative w-50 h-123 rounded-lg border-2 border-gray-200 bg-white shadow-md hover:shadow-2xl hover:border-blue-500 transition-all duration-200 select-none">
-    <Image src={product.main_image} alt={product.name} className="w-full aspect-5/4 rounded-t-md" />
+    <Image src={product.main_image} alt={product.name} width={50} height={123} className="w-full aspect-5/4 rounded-t-md" />
     <Link href={`/product/${product.slug}`} className="block px-3">
       <section className="mt-2">
-        <p className="font-medium">{product.name}</p>
+        <p className="font-medium max-h-12">{product.name}</p>
         <div className="mt-1 flex flex-row gap-1">
           <svg className="w-6 h-6 text-red-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor" viewBox="0 0 24 24">
             <path d="M8.597 3.2A1 1 0 0 0 7.04 4.289a3.49 3.49 0 0 1 .057 1.795 3.448 3.448 0 0 1-.84 1.575.999.999 0 0 0-.077.094c-.596.817-3.96 5.6-.941 10.762l.03.049a7.73 7.73 0 0 0 2.917 2.602 7.617 7.617 0 0 0 3.772.829 8.06 8.06 0 0 0 3.986-.975 8.185 8.185 0 0 0 3.04-2.864c1.301-2.2 1.184-4.556.588-6.441-.583-1.848-1.68-3.414-2.607-4.102a1 1 0 0 0-1.594.757c-.067 1.431-.363 2.551-.794 3.431-.222-2.407-1.127-4.196-2.224-5.524-1.147-1.39-2.564-2.3-3.323-2.788a8.487 8.487 0 0 1-.432-.287Z" />
