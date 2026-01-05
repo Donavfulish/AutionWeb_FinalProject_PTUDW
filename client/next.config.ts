8d3467930d7bc14b4c5bc725cf21b80b3c2769cf
MinhTri27343 <minhtri26072005@gmail.com>
2026-01-05 16:12:53 +0700

    fix: install lightningcss linux binary for vercel deploy

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,23 +2,23 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    reactCompiler: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: '**',
-            }
-        ]
-    },
-    turbopack: {
-        root: path.resolve(__dirname, "..")
-    }
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  turbopack: {
+    root: path.resolve(__dirname, ".."),
+  },
 };
 
 export default nextConfig;
